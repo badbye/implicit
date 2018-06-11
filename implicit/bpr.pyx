157cf6a5fe1b920a3a3fd0c6745b83c16b5ca095
duyalei <yaleidu93@gmail.com>
2018-06-11 16:00:15 +0800

    add bpr_favor_update

--- implicit/bpr.pyx
+++ implicit/bpr.pyx
@@ -236,7 +236,7 @@ def bpr_update(RNGVector rng,
                bool verify_neg):
     cdef integral users = X.shape[0], items = Y.shape[0]
     cdef long samples = len(userids), i, liked_index, disliked_index, correct = 0, skipped = 0
-    cdef integral j, liked_id, disliked_id, thread_id
+    cdef integral j, liked_id, disliked_id, thread_id, user_id
     cdef floating z, score, temp
 
     cdef floating * user
@@ -251,17 +251,211 @@ def bpr_update(RNGVector rng,
         for i in prange(samples, schedule='guided'):
             liked_index = rng.generate(thread_id)
             liked_id = itemids[liked_index]
+            user_id = userids[liked_index]
 
             # if the user has liked the item, skip this for now
             disliked_index = rng.generate(thread_id)
             disliked_id = itemids[disliked_index]
 
-            if verify_neg and has_non_zero(indptr, itemids, userids[liked_index], disliked_id):
+            if verify_neg and has_non_zero(indptr, itemids, user_id, disliked_id):
                 skipped += 1
                 continue
 
             # get pointers to the relevant factors
-            user, liked, disliked = &X[userids[liked_index], 0], &Y[liked_id, 0], &Y[disliked_id, 0]
+            user, liked, disliked = &X[user_id, 0], &Y[liked_id, 0], &Y[disliked_id, 0]
+
+            # compute the score
+            score = 0
+            for j in range(factors + 1):
+                score = score + user[j] * (liked[j] - disliked[j])
+            z = 1.0 / (1.0 + exp(score))
+
+            if z < .5:
+                correct += 1
+
+            # update the factors via sgd.
+            for j in range(factors):
+                temp = user[j]
+                user[j] += learning_rate * (z * (liked[j] - disliked[j]) - reg * user[j])
+                liked[j] += learning_rate * (z * temp - reg * liked[j])
+                disliked[j] += learning_rate * (-z * temp - reg * disliked[j])
+
+            # update item bias terms (last column of factorized matrix)
+            liked[factors] += learning_rate * (z - reg * liked[factors])
+            disliked[factors] += learning_rate * (-z - reg * disliked[factors])
+
+    return correct, skipped
+
+
+class BayesianPersonalizedFavorRanking(BayesianPersonalizedRanking):
+    """ Bayesian Personalized Ranking
+
+    A recommender model that learns  a matrix factorization embedding based off minimizing the
+    pairwise ranking loss described in the paper `BPR: Bayesian Personalized Ranking from Implicit
+    Feedback <https://arxiv.org/pdf/1205.2618.pdf>`_.
+    Update: only compare the favored items
+
+    Parameters
+    ----------
+    factors : int, optional
+        The number of latent factors to compute
+    learning_rate : float, optional
+        The learning rate to apply for SGD updates during training
+    regularization : float, optional
+        The regularization factor to use
+    dtype : data-type, optional
+        Specifies whether to generate 64 bit or 32 bit floating point factors
+    use_gpu : bool, optional
+        Fit on the GPU if available
+    iterations : int, optional
+        The number of training epochs to use when fitting the data
+    verify_same: bool, optional
+        When sampling negative items, check if the randomly picked negative item has actually
+        been liked by the user. This check increases the time needed to train but usually leads
+        to better predictions.
+    num_threads : int, optional
+        The number of threads to use for fitting the model. This only
+        applies for the native extensions. Specifying 0 means to default
+        to the number of cores on the machine.
+
+    Attributes
+    ----------
+    item_factors : ndarray
+        Array of latent factors for each item in the training set
+    user_factors : ndarray
+        Array of latent factors for each user in the training set
+    """
+    def __init__(self, factors=100, learning_rate=0.01, regularization=0.01, dtype=np.float32,
+                 iterations=100, num_threads=0, verify_same=True):
+        super(BayesianPersonalizedFavorRanking, self).__init__()
+
+
+        self.factors = factors
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.regularization = regularization
+        self.dtype = dtype
+        self.num_threads = num_threads
+        self.verify_same = verify_same
+        self.show_progress = True
+
+    @cython.cdivision(True)
+    @cython.boundscheck(False)
+    def fit(self, item_users):
+        """ Factorizes the item_users matrix
+
+        Parameters
+        ----------
+        item_users: coo_matrix
+            Matrix of confidences for the liked items. This matrix should be a coo_matrix where
+            the rows of the matrix are the item, and the columns are the users that liked that item.
+            BPR ignores the weight value of the matrix right now - it treats non zero entries
+            as a binary signal that the user liked the item.
+        """
+        # for now, all we handle is float 32 values
+        if item_users.dtype != np.float32:
+            item_users = item_users.astype(np.float32)
+
+        items, users = item_users.shape
+
+        # We need efficient user lookup for case of removing own likes
+        # TODO: might make more sense to just changes inputs to be users by items instead
+        # but that would be a major breaking API change
+        user_items = item_users.T.tocsr()
+        if not user_items.has_sorted_indices:
+            user_items.sort_indices()
+
+        # this basically calculates the 'row' attribute of a COO matrix
+        # without requiring us to get the whole COO matrix
+        user_counts = np.ediff1d(user_items.indptr)
+        userids = np.repeat(np.arange(users), user_counts).astype(user_items.indices.dtype)
+
+        # create factors if not already created.
+        # Note: the final dimension is for the item bias term - which is set to a 1 for all users
+        # this simplifies interfacing with approximate nearest neighbours libraries etc
+        if self.item_factors is None:
+            self.item_factors = (np.random.rand(items, self.factors + 1).astype(self.dtype) - .5)
+            self.item_factors /= self.factors
+
+            # set factors to all zeros for items without any ratings
+            item_counts = np.bincount(user_items.indices, minlength=items)
+            self.item_factors[item_counts == 0] = np.zeros(self.factors + 1)
+
+        if self.user_factors is None:
+            self.user_factors = (np.random.rand(users, self.factors + 1).astype(self.dtype) - .5)
+            self.user_factors /= self.factors
+
+            # set factors to all zeros for users without any ratings
+            self.user_factors[user_counts == 0] = np.zeros(self.factors + 1)
+
+            self.user_factors[:, self.factors] = 1.0
+
+        # we accept num_threads = 0 as indicating to create as many threads as we have cores,
+        # but in that case we need the number of cores, since we need to initialize RNG state per
+        # thread. Get the appropiate value back from openmp
+        cdef int num_threads = self.num_threads
+        if not num_threads:
+            num_threads = multiprocessing.cpu_count()
+
+        # initialize RNG's, one per thread.
+        cdef RNGVector rng = RNGVector(num_threads, len(user_items.data) - 1)
+        log.debug("Running %i BPR training epochs", self.iterations)
+        with tqdm.tqdm(total=self.iterations, disable=not self.show_progress) as progress:
+            for epoch in range(self.iterations):
+                correct, skipped = bpr_favor_update(rng, userids, user_items.indices, user_items.indptr,
+                                                   self.user_factors, self.item_factors,
+                                                   self.learning_rate, self.regularization, num_threads,
+                                                   self.verify_same, user_items.data)
+                progress.update(1)
+                total = len(user_items.data)
+                progress.set_postfix({"correct": "%.2f%%" % (100.0 * correct / (total - skipped)),
+                                      "skipped": "%.2f%%" % (100.0 * skipped / total)})
+
+
+@cython.cdivision(True)
+@cython.boundscheck(False)
+def bpr_favor_update(RNGVector rng,
+               integral[:] userids, integral[:] itemids, integral[:] indptr,
+               floating[:, :] X, floating[:, :] Y,
+               float learning_rate, float reg, int num_threads,
+               bool verify_neg,
+               integral[:] score_vec):
+    cdef integral users = X.shape[0], items = Y.shape[0]
+    cdef long samples = len(userids), i, liked_index, disliked_index, correct = 0, skipped = 0
+    cdef integral j, liked_id, disliked_id, thread_id, user_id
+    cdef floating z, score, temp, liked_score, disliked_score
+
+    cdef floating * user
+    cdef floating * liked
+    cdef floating * disliked
+
+    cdef integral factors = X.shape[1] - 1
+
+    with nogil, parallel(num_threads=num_threads):
+
+        thread_id = get_thread_num()
+        for i in prange(samples, schedule='guided'):
+            liked_index = rng.generate(thread_id)
+            liked_id = itemids[liked_index]
+            user_id = userids[liked_index]
+
+            disliked_index = np.random.sample(indptr[user_id], indptr[user_id + 1]+1)
+            disliked_id = itemids[disliked_index]
+
+            # if disliked == liked, skip
+            if verify_neg and disliked_index == liked_index:
+                skipped += 1
+                continue
+
+            liked_score = score_vec[liked_index]
+            disliked_score = score_vec[disliked_index]
+            if liked_score < disliked_score:
+                liked_id, disliked_id = disliked_id, liked_id
+
+
+            # get pointers to the relevant factors
+            user, liked, disliked = &X[user_id, 0], &Y[liked_id, 0], &Y[disliked_id, 0]
+
 
             # compute the score
             score = 0
